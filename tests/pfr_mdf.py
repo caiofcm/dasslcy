c6e973c0b9fa9a850aa480e01bc2467eae0a36aa
Caio Marcellos <caiocuritiba@gmail.com>
2018-06-11 12:29:09 -0300

    refactor pfr model

--- tests/pfr_mdf.py
+++ tests/pfr_mdf.py
@@ -8,7 +8,29 @@ except ImportError:
 import dasslcy
 from numba import jit
 
-# TODO: Numba: Use a jitclass for comparison with pfr
+def base_model_calculations(res, y, yp, N, D, vz, k, Cf, h):
+    dCi = yp
+    Ci = y
+    aux1 = D / (vz * h)
+    C0 = 1.0 / (1.0 + aux1) * (aux1 * Ci[0] + Cf)
+    CNp1 = Ci[N - 1]
+    #r[:] = k * Ci
+    aux2 = D / h**2
+    aux3 = vz / (2 * h)
+    res[0] = aux2 * (Ci[1] - 2.0 * Ci[0] + C0) - \
+        aux3 * (Ci[1] - C0) + k * Ci[0] - dCi[0]
+    for i in np.arange(1, N - 1):
+        tt1 = aux2 * (Ci[i + 1] - 2.0 * Ci[i] + Ci[i - 1])
+        tt2 = -aux3 * (Ci[i + 1] - Ci[i - 1]) + k * Ci[i]
+        res[i] = tt1 + tt2 - dCi[i]
+    res[N - 1] = aux2 * (CNp1 - 2.0 * Ci[N - 1] + Ci[N - 2]) - \
+        aux3 * (CNp1 - Ci[N - 2]) + k * Ci[N - 1] - dCi[N - 1]
+    #ires = 0
+    #return res, ires
+    pass
+
+def get_h(zf, z0, intervals):
+        return (zf - z0) / intervals
 
 class pfr():
     D = 1.0
@@ -30,90 +52,44 @@ class pfr():
     def initialize_intermediaries(self):
         self.r = np.empty(self.N)
 
-def pfr_as_list(par):
-    pfr_list_version = [par.D, par.vz, par.k, par.Cf, par.N, par.h]
-    return pfr_list_version
-
 def model_pfr(t, y, yp, par):
     res = np.empty(par.N)
-    r = par.r
+    #r = par.r
     N = par.N
-    dCi = yp
-    Ci = y
-    aux1 = par.D / (par.vz * par.h)
-    C0 = 1.0/(1.0 + aux1) * (aux1*Ci[0] + par.Cf)
-    CNp1 = Ci[N - 1]
-    r[:] = par.k*Ci
-    aux2 = par.D / par.h**2
-    aux3 = par.vz / (2 * par.h)
-    res[0] = aux2 * (Ci[1] - 2.0 * Ci[0] + C0) - aux3 * (Ci[1] - C0) + r[0] - dCi[0]
-    for i in np.arange(1, N - 1):
-        tt1 = aux2 * (Ci[i + 1] - 2.0 * Ci[i] + Ci[i - 1])
-        tt2 = -aux3 * (Ci[i+1] - Ci[i-1]) + r[i]
-        res[i] = tt1 + tt2 - dCi[i]
-    res[N-1] = aux2 * (CNp1 - 2.0 * Ci[N-1] + Ci[N-2]) - aux3 * (CNp1 - Ci[N - 2]) + r[N - 1] - dCi[N-1]
-    ires = 0
-    return res, ires
+    base_model_calculations(
+            res, y, yp, N, par.D, par.vz, par.k, par.Cf, par.h)
+    return res, 0
 
 def model_pfr_shared(t, y, yp, par, res):
-    r = par.r
+    #r = par.r
     N = par.N
-    dCi = yp
-    Ci = y
-    aux1 = par.D / (par.vz * par.h)
-    C0 = 1.0 / (1.0 + aux1) * (aux1 * Ci[0] + par.Cf)
-    CNp1 = Ci[N - 1]
-    r[:] = par.k * Ci
-    aux2 = par.D / par.h**2
-    aux3 = par.vz / (2 * par.h)
-    res[0] = aux2 * (Ci[1] - 2.0 * Ci[0] + C0) - \
-        aux3 * (Ci[1] - C0) + r[0] - dCi[0]
-    for i in np.arange(1, N - 1):
-        tt1 = aux2 * (Ci[i + 1] - 2.0 * Ci[i] + Ci[i - 1])
-        tt2 = -aux3 * (Ci[i + 1] - Ci[i - 1]) + r[i]
-        res[i] = tt1 + tt2 - dCi[i]
-    res[N - 1] = aux2 * (CNp1 - 2.0 * Ci[N - 1] + Ci[N - 2]) - \
-        aux3 * (CNp1 - Ci[N - 2]) + r[N - 1] - dCi[N - 1]
-    ires = 0
-    return res, ires
+    base_model_calculations(
+            res, y, yp, N, par.D, par.vz, par.k, par.Cf, par.h)
+    return res, 0
 
-# def model_pfr_numba(t, y, yp):
-#     D = 1.0
-#     vz = 1.0
-#     k = 1.0
-#     Cf = 1.0
-#     N = 20
-#     h = 1.0
-#     # r = par[6]
-#     r = np.empty(N)
-#     res = np.empty(N)
+def numba_model_pfr(N):
+    D = 1.0
+    vz = 1.0
+    k = 1.0
+    Cf = 1.0
+    z0 = 0.0
+    zf = 1.0
+    h = get_h(zf, z0, N)
 
-#     dCi = yp
-#     Ci = y
-#     aux1 = D / (vz * h)
-#     C0 = 1.0 / (1.0 + aux1) * (aux1 * Ci[0] + Cf)
-#     CNp1 = Ci[N - 1]
-#     r[:] = k * Ci
-#     aux2 = D / h**2
-#     aux3 = vz / (2 * h)
-#     res[0] = aux2 * (Ci[1] - 2.0 * Ci[0] + C0) - \
-#         aux3 * (Ci[1] - C0) + r[0] - dCi[0]
-#     for i in np.arange(1, N - 1):
-#         tt1 = aux2 * (Ci[i + 1] - 2.0 * Ci[i] + Ci[i - 1])
-#         tt2 = -aux3 * (Ci[i + 1] - Ci[i - 1]) + r[i]
-#         res[i] = tt1 + tt2 - dCi[i]
-#     res[N - 1] = aux2 * (CNp1 - 2.0 * Ci[N - 1] + Ci[N - 2]) - \
-#         aux3 * (CNp1 - Ci[N - 2]) + r[N - 1] - dCi[N - 1]
-#     ires = 0
-#     return res, ires
-#     return 0, 0
+    jitted_base_model = jit(base_model_calculations, nopython=True)
 
-# def model_numba_jit():
-#     model_pfr_jit = jit(model_pfr_numba, nopython=True)
-#     return model_pfr_jit
+    def model_nmb_pfr(t, y, yp):
+        res = np.empty(N)
+        jitted_base_model(
+            res, y, yp, N, D, vz, k, Cf, h)
+        ires = 0
+        return res, ires
+    return model_nmb_pfr
 
-######################################### Solve model2 ###################
-    #print('------- Solve model_pfr  ---------- ')
+def model_numba_jit(N = 20):
+    func = numba_model_pfr(N)
+    model_pfr_jit = jit(func, nopython=True)
+    return model_pfr_jit
 
 #--------------------------------------------
 # 	 SETUPS
@@ -134,26 +110,39 @@ def setup_solver_regular(N):
 def setup_solver_shared(N):
     return tuple([model_pfr_shared] + setup_base(N))
 
-# def setup_solver_numba(N):
-#     spt = setup_base(N)
-#     spt[3] = None
-#     jitted_model = model_numba_jit()
-#     #Test
-#     jitted_model(0.0, np.zeros(20), np.array(20))
-#     return tuple([jitted_model] + spt)
+def setup_solver_numba(N):
+    spt = setup_base(N)
+    spt[3] = None
+    jitted_model = model_numba_jit(N)
+    #Test
+    #jitted_model(0.0, np.zeros(20), np.array(20))
+    return tuple([jitted_model] + spt)
 
 def run_solver(N):
     args = setup_solver_regular(N)
     dasslcy.solve(*args)
     return
 
-# def run_solver_numba(N):
-#     args = setup_solver_numba(N)
-#     dasslcy.solve(*args)
+def run_solver_numba(N):
+    args = setup_solver_numba(N)
+    dasslcy.solve(*args)
 
 #--------------------------------------------
 # 	 TIMING FUNCS
 #--------------------------------------------
+
+def run_timeit(stmt, setup, rpt_args, logic = None):
+    tmr = timeit.Timer(stmt, setup).repeat(*rpt_args)
+    if logic is None:
+        val = min(tmr)
+    else:
+        val = logic(tmr)
+    #print(tmr)
+    return val
+
+from functools import partial
+run_timeit_dischard_first = partial(run_timeit,
+        logic=lambda v: min(v[1:]) if len(v) > 1 else v[0])
 
 def timing_regular(N, rpt_args):
     setup_str = '''
@@ -161,7 +150,7 @@ from __main__ import setup_solver_regular, dasslcy
 args = setup_solver_regular({})
     '''.format(N)
     # tt = timeit.timeit('dasslcy.solve(*args)', setup=setup_str, number=1)
-    tt = run_timeit('dasslcy.solve(*args)', setup=setup_str,
+    tt = run_timeit_dischard_first('dasslcy.solve(*args)', setup=setup_str,
                     rpt_args=rpt_args)
     print('Regular solver: {} s'.format(tt))
 
@@ -170,31 +159,26 @@ def timing_shared(N, rpt_args):
 from __main__ import setup_solver_shared, dasslcy;
 args = setup_solver_shared({})
     '''.format(N)
-    tt = run_timeit('dasslcy.solve(*args, share_res=1)',
+    tt = run_timeit_dischard_first('dasslcy.solve(*args, share_res=1)',
                     setup=setup_str, rpt_args=rpt_args)
     print('Shared mem solver: {} s'.format(tt))
 
-# def timing_numba(N, rpt_args):
-#     setup_str = '''
-# from __main__ import setup_solver_numba, dasslcy;
-# args = setup_solver_numba({})
-#     '''.format(N)
-#     tt = run_timeit('dasslcy.solve(*args)',
-#                     setup=setup_str, rpt_args=rpt_args)
-#     print('DASSLCY + NUMBA: {} s'.format(tt))
+def timing_numba(N, rpt_args):
+    setup_str = '''
+from __main__ import setup_solver_numba, dasslcy;
+args = setup_solver_numba({})
+    '''.format(N)
+    tt = run_timeit_dischard_first('dasslcy.solve(*args)',
+                    setup=setup_str, rpt_args=rpt_args)
+    print('DASSLCY + NUMBA: {} s'.format(tt))
 
-def run_timeit(stmt, setup, rpt_args):
-    tmr = timeit.Timer(stmt, setup).repeat(*rpt_args)
-    min_val = min(tmr)
-    #print(tmr)
-    return min_val
 
 def timing():
-    N = 20
-    args_tt = (N, (1, 1))
+    N = 100
+    args_tt = (N, (2, 1))
     timing_regular(*args_tt)
     timing_shared(*args_tt)
-    # timing_numba(*args_tt)
+    timing_numba(*args_tt)
 
 
 
